7ba2be26a19a51b2e1b20e2910cf125b857d34e2
simacna <simacna@gmail.com>
2015-04-25 10:57:08 -0400

    pushing yesterday work before continuing

--- color.js
+++ color.js
@@ -1,25 +1,27 @@
 var main = function() {
-        var canvas = document.getElementById('canvas'),
-                context = canvas.getContext('2d');
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
 
-        // resize the canvas to fill browser window dynamically
-        window.addEventListener('resize', resizeCanvas, false);
-        
-        function resizeCanvas() {
-                canvas.width = window.innerWidth;
-                canvas.height = window.innerHeight;
-                
-                /**
-                 * Your drawings need to be inside this function otherwise they will be reset when 
-                 * you resize the browser window and the canvas goes will be cleared.
-                 */
-                drawStuff(); 
-        }
-        resizeCanvas();
-        
-        function drawStuff() {
-                // do your drawing stuff here
-        }
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', resizeCanvas, false);
+    
+function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+            
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when 
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    drawStuff(); 
+}
+    resizeCanvas();
+
+    function drawStuff() {
+            // do your drawing stuff here
+    }
+
+    
 };
 
 $(document).ready(main);
